d642ffff96ec0e3e5acd6253ecdd0c886b9291c5
EnderArch2 <enderarcherboy+tag@gmail.com>
2026-01-16 13:20:51 +0700

    hello world

--- commands/start.py
+++ commands/start.py
@@ -4,4 +4,4 @@ from telegram.ext import ContextTypes
 DESCRIPTION = "Start the bot and get welcome message"
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello! This logic is loaded from start.py")
+    await update.message.reply_text("Hello World!")
