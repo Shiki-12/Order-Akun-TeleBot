c204fb2dee61560e48684fed2b5a34f278f30438
Shiki-12 <uknowndonp@gmail.com>
2026-01-17 13:08:57 +0700

    perbaiki sedikit back_to_start

--- callbacks/back_to_start.py
+++ callbacks/back_to_start.py
@@ -1,23 +1,44 @@
 import db
+import config
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ContextTypes
 
 PATTERN = "^back_to_start$"
 
 async def callback_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
+    query = update.callback_query 
     await query.answer()
 
+    user = update.effective_user
+    user_id = user.id
+    username = f"@{user.username}" if user.username else "Tidak ada"
+    
+   
     total_stok = db.get_total_accounts_count()
-    caption_start = (
-        f"👋 <b>Halo {update.effective_user.first_name}!</b>\n"
-        f"Selamat Datang Kembali di <b>Premium Store</b>\n\n"
+
+    file_id = config.BANNER_FILE_ID 
+
+    caption_text = (
+        f"👋 <b>Halo {user.first_name}!</b>\n"
+        f"Selamat Datang di <b>Premium Store</b>\n\n"
+        f"🆔 <b>User Info:</b>\n"
+        f"├ ID: <code>{user_id}</code>\n"
+        f"├ Username: {username}\n"
+        f"└ Transaksi: Rp 0\n\n"
         f"📊 <b>Bot Stats:</b>\n"
+        f"├ Terjual: 0 pcs\n"
+        f"├ Total Transaksi: Rp 0\n"
         f"└ Stok Ready: <code>{total_stok}</code> Akun\n\n"
-        f"Silakan pilih menu di bawah ini:"
+        f"🚀 <b>Shortcuts:</b>\n"
+        f"/accounts - Untuk melihat stok produk"
     )
     reply_markup = InlineKeyboardMarkup([
         [InlineKeyboardButton("🛒 List Produk", callback_data='list_produk')],
         [InlineKeyboardButton("❓ Cara Order", callback_data='how_to_order')]
     ])
-    await query.edit_message_caption(caption=caption_start, parse_mode='HTML', reply_markup=reply_markup)
+
+    await query.edit_message_caption(
+        caption=caption_text, 
+        parse_mode='HTML', 
+        reply_markup=reply_markup
+    )
